e4108144ff4e3295715ccb245e373f76afaa7de0
Abhishek Kulkarni <kabhis2002@gmail.com>
2022-03-31 23:06:27 +0530

    firebase and moralis integeration successful

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 import {useState} from 'react'
 import { initializeApp } from "firebase/app";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
 
 function App() {
+  const Moralis = require('moralis');
+  const serverUrl = 'https://mnz3ceef8fuw.usemoralis.com:2053/server';
+  const appId= 'H5ZMxViMSncav4x9MprMJd3Bg3ZJfiyGo84OuO4h';
+  Moralis.start({serverUrl,appId});
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [user_id, setUser] = useState('');
+  const [fName, setFname] = useState('');
+  const [lName, setLname] = useState('');
+  const [addr,setAddr] = useState('');
+
   var firebaseConfig = {
     apiKey: "AIzaSyDzGnYjcD6QCOKRLT_shOkDg_g-_HCU6V4",
   authDomain: "payapp-5208c.firebaseapp.com",
@@ -28,6 +39,7 @@ function App() {
   .then((userCredential) => {
     // Signed in 
     const user = userCredential.user;
+    writeUserData(user_id,fName,lName,email,addr);
     // ...
   })
   .catch((error) => {
@@ -36,21 +48,73 @@ function App() {
     // ..
   });
   }
+
+  function writeUserData(user_id, fName, lName, email, addr) {
+    const db = getDatabase();
+    set(ref(db, 'users/' + user_id), {
+      first_name: fName,
+      last_name: lName,
+      email: email,
+      address : addr
+    });
+  }
+  async function login() {
+
+    let user = Moralis.User.current();
+    if (!user) {
+      user = await Moralis.authenticate({
+        signingMessage: "Welcome",
+      })
+        .then(function (user) {
+          setAddr(user.get('ethAddress'))
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
   return(
-    <div>
+    <div className='App'>
+      <div>
       <form onSubmit={(e)=>{submit(e)}} >
         <label htmlFor="#user">
-          Username:- 
-          <input type="email" id='user' onChange={(e)=>{setEmail(e.target.value)}} />
+          Username
+          <br />
+          <input type="text" id='user' onChange={(e)=>{setUser(e.target.value)}} />
         </label>
         <br />
+
+        <label htmlFor="#f_name">
+          First Name
+          <br />
+          <input type="text" id='f_name' onChange={(e)=>{setFname(e.target.value)}} />
+        </label>
+        <br />
+
+        <label htmlFor="#l_name">
+          Last Name
+          <br />
+          <input type="text" id='l_name' onChange={(e)=>{setLname(e.target.value)}} />
+        </label>
+        <br />
+
+        <label htmlFor="#user_id">
+          Email
+          <br />
+          <input type="email" id='user_id' onChange={(e)=>{setEmail(e.target.value)}} />
+        </label>
+        <br />
+
         <label htmlFor="#password">
-          Password:- 
+          Password
+          <br />
           <input type="password" onChange={(e)=>setPassword(e.target.value)}/>
         </label>
         <br />
+        <button onClick={login}>Get Metamask wallet</button>
         <input type="submit" value="submit" />
       </form>
+      </div>
     </div>
   )
 }
